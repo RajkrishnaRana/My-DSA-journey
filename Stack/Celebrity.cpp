e1f0b931b769b0cd609d71562909227fb3f72c86
RajkrishnaRana <95145115+RajkrishnaRana@users.noreply.github.com>
2022-09-15 07:05:25 +0530

    Create Celebrity.cpp
    GFG practice

---
+++ Stack/Celebrity.cpp
@@ -0,0 +1,49 @@
+class Solution 
+{
+    private:
+    bool knows(vector<vector<int> >& M, int a, int b) {
+        if (M[a][b] == 1) return true;
+        else return false;
+    }
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack <int> s;
+        for (int i = 0; i < n; i ++) {
+            s.push(i);
+        }
+        
+        while (s.size() > 1) {
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if (knows (M, a, b))
+                s.push(b);
+            else
+                s.push(a);
+        }
+        
+        int candidate = s.top();
+        bool rowCheck = false;
+        int count = 0;
+        for (int i = 0; i < n; i ++) {
+            if (M[candidate][i] == 0)
+                count ++;
+        }
+        if (count != n) return -1;
+        
+        bool colCheck = false;
+        count = 0;
+        for (int i = 0; i < n; i ++) {
+            if (M[i][candidate] == 1)
+                count ++;
+        }
+        if (count != n - 1) return -1;
+        
+        return candidate;
+    }
+};
